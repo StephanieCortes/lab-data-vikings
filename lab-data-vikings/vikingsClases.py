4153a3a74a7d31870d3cfaacef713e9a54192c77
StephanieCortes <step_0707@hotmail.com>
2020-10-18 22:07:18 -0500

    terminé pero no hice el bonus y tampoco pude correr las pruebas

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -3,19 +3,47 @@
 
 
 class Soldier:
-    pass
+    def __init__(self,health,strenght):
+    self.health=health
+    self.strenght=strenght
+    
+    def attack(self):
+        return strenght
+    
+    def receiveDamage(self,damage):
+        self.damage= self.health-damage
+    
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self,name,health,strenght):
+        self.name=name
+        self.health=health
+        self.strenght=strenght
+    
+    def receiveDamage(self,damage):
+        self.damage=self.health-damage
+            if self.damage <self.health:
+                print(self.name," has received",self.damage,"points of damage")
+            else:
+                print(self.name," has died in combat")
+    
+    def BattleCry(self)
+        return "Odin Owns You All!"
+
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def receiveDamage(self,damage):
+        self.damage=self.health-damage
+            if self.damage <self.health:
+                print('A Saxon has received',self.damage,' points of damage')
+            else:
+                print("A Saxon has died in combat")
 
 # War
 
